22bbde919a936999cb0d0e6d2849701cf348e9cf
Eero Vilpponen <eero.vilpponen@gmail.com>
2022-04-14 10:20:09 +0300

    Draw links

--- app.js
+++ app.js
@@ -2,10 +2,11 @@
 function loadApp() {
     // Load the app
     showFirstImage();
+    window.breadCrumbs = [];
 }
 
 function showFirstImage() {
-    const firstImageId = window.database.images[0].id;
+    const firstImageId = Object.keys(window.database.images)[0];
     const el = document.querySelector('#first-image');
     if (el) {
       el.src = localStorage.getItem(firstImageId);
@@ -21,10 +22,56 @@ function startApp() {
     const appEl = document.querySelector('#app-container');
     appEl.classList.remove('hidden');
 
-    setMainImage(localStorage.getItem(window.database.images[0].id));
+    setMainImage(window.database.images[Object.keys(window.database.images)[0]]);
 }
 
-function setMainImage(src) {
+function drawLinks() {
+    const links = document.querySelectorAll('#overlay .link');
+    // Delete existing links
+    links.forEach(link => {
+        link.remove();
+    });
+    for (const link of (window.mainImage.links || [])) {
+        const el = document.createElement('div');
+        el.classList.add('link');
+        el.innerHTML = '<h2>LINK</h2>';
+        el.style.top = link.top + 'vh';
+        el.style.left = link.left + 'vh';
+        el.addEventListener('click', () => {
+            setMainImage(window.database.images[link.id]);
+        });
+        document.querySelector('#overlay').appendChild(el);
+    }
+}
+
+function drawBackButton() {
+    if (window.breadCrumbs.length < 2) {
+        console.log("cant go back")
+        return;
+    }
+    const el = document.createElement('div');
+    el.classList.add('link');
+    el.innerHTML = '<h2>BACK</h2>';
+    el.style.top = '1%';
+    el.style.left = '1%';
+    el.addEventListener('click', () => {
+        window.breadCrumbs.pop();
+        setMainImage(window.breadCrumbs.pop());
+    });
+    document.querySelector('#overlay').appendChild(el);
+}
+
+function refreshOverlay() {
+    drawLinks();
+    drawBackButton();
+}
+
+function setMainImage(image) {
+    window.mainImage = image;
+    const id = image.id;
+    const src = localStorage.getItem(id);
     const el = document.querySelector('#main-image');
     el.src = src;
+    window.breadCrumbs.push(window.mainImage);
+    refreshOverlay();
 }
